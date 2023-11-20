e7ff76553bca1c09f10f635669bca57600c11292
aaron-watts <aaron.watts@murena.io>
2023-11-20 10:31:39 +0000

    define sequelize model

---
+++ src/models/image.model.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Image = sequelize.define("image", {
+        type: {
+            type: DataTypes.STRING
+        },
+        name: {
+            type: DataTypes.STRING
+        },
+        data: {
+            type: DataTypes.BLOB("long")
+        }
+    });
+
+    return Image;
+};
