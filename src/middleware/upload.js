948532f42c9752550a1fd85cc37266328dbe3c18
aaron-watts <aaron.watts@murena.io>
2023-11-26 11:32:37 +0000

    fix typo

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 
 const imageFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("imaage")) {
+    if (file.mimetype.startsWith("image")) {
         cb(null, true);
     } else {
         cb("Please upload only images.", false)
