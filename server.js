59b8744f2e00f52690fa6263d17ece485c0d3135
aaron-watts <aaron.watts@murena.io>
2023-12-03 14:47:54 +0000

    remove commented code

--- server.js
+++ server.js
@@ -5,17 +5,15 @@ const app = express();
 const db = require("./src/models");
 const initRoutes = require("./src/routes/web");
 
-// global.__baserdir = __dirname;
-
 app.use(express.static('./resources/static'))
 
 app.use(express.urlencoded({ extended: true }));
 initRoutes(app);
 
-// db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-    console.log("Drop and re-sync db.")
-});
+db.sequelize.sync();
+// db.sequelize.sync({ force: true }).then(() => {
+//     console.log("Drop and re-sync db.")
+// });
 
 let port = 3000;
 app.listen(port, () => {
