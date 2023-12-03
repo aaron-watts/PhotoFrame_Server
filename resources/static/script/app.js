a2ad0f74e464de686bc75725581756436e908a2d
aaron-watts <aaron.watts@murena.io>
2023-12-03 16:18:48 +0000

    add overlay delete form for fetched images

--- resources/static/script/app.js
+++ resources/static/script/app.js
@@ -33,17 +33,45 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(res => res.json())
         .then(json => {
             loader.classList.add('loader-hide');
-            
-            for (let image of json) {
-                const baseURL = '/assets/uploads/';
 
-                const newLi = document.createElement('li');
-                const img = document.createElement('img');
-                img.src = `${baseURL}${image.name}`;
-                img.classList.add('thumbnail');
-
-                newLi.appendChild(img);
-                imageList.appendChild(newLi);
-            }
+            for (let image of json) 
+            imageList.appendChild(buildImageThumbnail(image));
         });
 });
+
+const buildImageThumbnail = image => {
+    const baseURL = '/assets/uploads/';
+    const svgHTML = `
+        <svg viewBox="0 -960 960 960" width="68%">
+            <path
+                fill="rgb(200, 0, 0)"
+                d="M280-120q-33 
+                0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 
+                33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 
+                0h80v-360h-80v360ZM280-720v520-520Z" 
+            />
+        </svg>`;
+
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('thumbnail-container');
+
+    const img = document.createElement('img');
+    img.src = `${baseURL}${image.name}`;
+    img.classList.add('thumbnail');
+
+    newDiv.appendChild(img);
+
+    const newForm = document.createElement('form');
+    newForm.classList.add('form-delete');
+    newForm.action = '';
+
+    const newBtn = document.createElement('button');
+    newBtn.classList.add('btn-delete');
+    newBtn.type = 'submit';
+    newBtn.innerHTML = svgHTML;
+
+    newForm.appendChild(newBtn);
+    newDiv.appendChild(newForm);
+
+    return newDiv;
+};
